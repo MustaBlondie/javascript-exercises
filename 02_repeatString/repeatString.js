8b1e660626030b658e7dd5cb6aff822638b746cb
Maria Morales <victorina23x@gmail.com>
2024-07-16 16:31:14 -0400

    add the first three practices

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(greeting, num) {
+    let cadena = '';
+    for(let i = 0; i < num; i++){
+        cadena += greeting;
+    }
+    return cadena;
 };
 
+repeatString('hey', 3);
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
-
-};
+const reverseString = function(greeting) {
+    let core = greeting.split('');
+    core.reverse();
+    let coreString = "";
+  
+    for(let i = 0; i < core.length; i++){
+      coreString += core[i];
+    }
+    
+    return coreString;
+  };
+  
+reverseString('hello');
 
 // Do not edit below this line
 module.exports = reverseString;
