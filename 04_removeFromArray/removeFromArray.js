24067877ec4d84f59fc69134655126e04fbe1b72
Maria Morales <victorina23x@gmail.com>
2024-07-17 16:22:52 -0400

    I finally finished exercise 4, I almost went bald...I don't know if it's the best way to approach it

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(cadena) {
+  let nuevoArray = [];
+  for(let i = 0; i < cadena.length; i++){
+      if(cadena[i] != 3){
+        nuevoArray.push(cadena[i]);
+      }
+  }
+  return nuevoArray;
+  };
+
+removeFromArray([1, 2, 3, 4], 3)
+    
 
 // Do not edit below this line
 module.exports = removeFromArray;
